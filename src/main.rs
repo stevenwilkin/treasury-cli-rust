8ab39767634bba7fc2bfd2de1e2093e80a7aa137
Steven Wilkin <stevenwilkin@gmail.com>
2023-06-16 18:49:45 +0700

    Read websocket details from `.env`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::process::exit;
+use dotenv::dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let url = match env::var("TREASURY_WS_URL") {
+        Ok(u) => u,
+        Err(_) => {
+            println!("TREASURY_WS_URL not set");
+            exit(1);
+        }
+    };
+
+    let auth_token = match env::var("TREASURY_AUTH_TOKEN") {
+        Ok(at) => at,
+        Err(_) => {
+            println!("TREASURY_AUTH_TOKEN not set");
+            exit(1);
+        }
+    };
+
+    println!("{}", url);
+    println!("{}", auth_token);
 }
